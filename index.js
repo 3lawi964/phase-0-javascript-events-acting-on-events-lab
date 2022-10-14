4085183cc2fbdf9a65f5a9101e67db9202df0710
alimajid <alidzk98@gmail.com>
2022-10-14 20:31:58 +0300

    Done git add .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const moveDodgerLeft = () => {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
   if (left > 0) {
-    dodger.style.left = `${left - 2}px`;
+    dodger.style.left = `${left - 1}px`;
   }
 };
 
@@ -16,7 +16,7 @@ const moveDodgerRight = () => {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
   if (left < 360) {
-    dodger.style.left = `${left + 2}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 };
 document.addEventListener("keydown", function (event) {
